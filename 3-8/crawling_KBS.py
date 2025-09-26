1e2e493083d07a452faafeff28fd5f3dbd5705cf
LTM0714 <lim2309043@gmail.com>
2025-09-26 14:53:04 +0900

    셀레니움을 사용해서 네이버 사이트에 로그인하고 필요 콘텐츠 검색

--- 3-8/crawling_KBS.py
+++ 3-8/crawling_KBS.py
@@ -1,44 +1,44 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
 def main(user_id, user_pw):
+    # 웹 드라이버의 버전 관리를 자동으로 해주는 ChromeDriverManager 사용
+    # Service 객체를 통해 드라이버 경로 설정
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    try:
-        # 네이버 로그인 페이지 열기
-        driver.get("https://nid.naver.com/nidlogin.login")
+    # 네이버 로그인 페이지 열기
+    driver.get('https://nid.naver.com/nidlogin.login')
 
-        # 로그인 정보 입력
-        driver.find_element(By.XPATH, '//*[@id="id"]').send_keys(user_id) # XPath는 웹페이지에서 요소를 선택하는데 사용되는 경로 표현식
-        driver.find_element(By.XPATH, '//*[@id="pw"]').send_keys(user_pw)
-        driver.find_element(By.ID, "log.login").click() # ID는 HTML 요소의 고유 식별자
+    # 로그인 정보 입력
+    driver.find_element(By.XPATH, '//*[@id="id"]').send_keys(user_id) # XPath는 웹페이지에서 요소를 선택하는데 사용되는 경로 표현식
+    driver.find_element(By.XPATH, '//*[@id="pw"]').send_keys(user_pw)
+    driver.find_element(By.ID, "log.login").click() # ID는 HTML 요소의 고유 식별자
 
-        time.sleep(20)  # 로그인 처리 대기
+    input('로그인 완료 후 Enter 키를 누르세요...')
 
-        # 네이버 메일 페이지로 이동
-        driver.get("https://mail.naver.com/")
-        time.sleep(3)
+    # 네이버 메일 페이지로 이동
+    driver.get('https://mail.naver.com/')
+    time.sleep(3)
 
-        # 메일 제목 크롤링
-        titles = []
-        mail_elements = driver.find_elements(By.CSS_SELECTOR, 'a.mail_title_link span.text') # CSS 선택자는 HTML 요소를 선택하는데 사용되는 패턴
-        for mail in mail_elements[:5]:  # 앞 5개만 가져오기
-            titles.append(mail.text)
+    # 메일 제목 크롤링
+    titles = []
+    mail_elements = driver.find_elements(By.CSS_SELECTOR, 'a.mail_title_link span.text') # CSS 선택자는 HTML 요소를 선택하는데 사용되는 패턴
+    for mail in mail_elements[:5]:  # 앞 5개만 가져오기
+        titles.append(mail.text)
 
-        return titles
+    driver.quit()
 
-    finally:
-        driver.quit()
+    return titles
 
 
-if __name__ == "__main__":
-    user_id = "dlaxoals14"
-    user_pw = "qwer1234!"
-    mail_titles = main(user_id, user_pw)
+if __name__ == '__main__':
+    id = 'dlaxoals14'
+    pw = 'qwer1234!'
+    mail_titles = main(id, pw)
     
-    print("내 메일 제목 목록:")
+    print('내 메일 제목 목록:')
     for idx, title in enumerate(mail_titles, start=1):
         print(f"{idx}. {title}")
