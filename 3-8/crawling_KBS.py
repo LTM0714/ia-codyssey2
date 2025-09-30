d41a6238bed0f7282910f536da626e2df157a76b
LTM0714 <lim2309043@gmail.com>
2025-09-30 09:39:31 +0900

    셀레니움을 사용해서 네이버 사이트에 로그인하고 필요 콘텐츠 검색

--- 3-8/crawling_KBS.py
+++ 3-8/crawling_KBS.py
@@ -2,50 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains, Keys
-import pyperclip
 import time
-import os
-from dotenv import load_dotenv
-
-# .env 파일 로드
-load_dotenv()
 
 def main(user_id, user_pw):
     # 웹 드라이버의 버전 관리를 자동으로 해주는 ChromeDriverManager 사용
     # Service 객체를 통해 드라이버 경로 설정
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get('https://nid.naver.com/nidlogin.login')
-    time.sleep(2)
 
-    # ClipBoard 복사/붙이기
-    driver.find_element(By.XPATH, '//*[@id="id"]').click()
-    pyperclip.copy(user_id)
-    actions = ActionChains(driver)
-    actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
-    time.sleep(2)
-    
-    pw_input = driver.find_element(By.XPATH, '//*[@id="pw"]')
-    pw_input.click()
-    pyperclip.copy(user_pw)
-    actions = ActionChains(driver)
-    actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
-    time.sleep(2)
-    
-    pw_input.send_keys(Keys.RETURN)
-    time.sleep(3)
-   
+    # 네이버 로그인 페이지 열기
+    driver.get('https://nid.naver.com/nidlogin.login')
+
+    # 로그인 정보 입력
+    driver.find_element(By.XPATH, '//*[@id="id"]').send_keys(user_id) # XPath는 웹페이지에서 요소를 선택하는데 사용되는 경로 표현식
+    driver.find_element(By.XPATH, '//*[@id="pw"]').send_keys(user_pw)
+    driver.find_element(By.ID, "log.login").click() # ID는 HTML 요소의 고유 식별자
+
+    input('로그인 완료 후 Enter 키를 누르세요...')
+
     nickname = driver.find_element(By.CSS_SELECTOR, '[class*="MyView-module__nickname"]').text
     email = driver.find_element(By.CSS_SELECTOR, '[class*="MyView-module__desc_email"]').text
     print(f'로그인된 닉네임: {nickname}')
     print(f'이메일 주소: {email}\n')
 
+    # 네이버 메일 페이지로 이동
     driver.get('https://mail.naver.com/')
     time.sleep(3)
 
+    # 메일 제목 크롤링
     titles = []
-    mail_elements = driver.find_elements(By.CSS_SELECTOR, 'a.mail_title_link span.text')
-    for mail in mail_elements[:5]:
+    mail_elements = driver.find_elements(By.CSS_SELECTOR, 'a.mail_title_link span.text') # CSS 선택자는 HTML 요소를 선택하는데 사용되는 패턴
+    for mail in mail_elements[:5]:  # 앞 5개만 가져오기
         titles.append(mail.text)
 
     driver.quit()
@@ -54,13 +40,10 @@ def main(user_id, user_pw):
 
 
 if __name__ == '__main__':
-    id = os.getenv('NAVER_ID')  # 또는 os.environ.get('NAVER_ID')
-    pw = os.getenv('NAVER_PW')
+    id = 'my_id'
+    pw = 'my_pw'
+    mail_titles = main(id, pw)
     
-    if not id or not pw:
-        print("환경변수 NAVER_ID 또는 NAVER_PW가 설정되지 않았습니다.")
-    else:
-        mail_titles = main(id, pw)
-        print('내 메일 제목 목록:')
-        for idx, title in enumerate(mail_titles, start=1):
-            print(f"{idx}. {title}")
+    print('내 메일 제목 목록:')
+    for idx, title in enumerate(mail_titles, start=1):
+        print(f"{idx}. {title}")
