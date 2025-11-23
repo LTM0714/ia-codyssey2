beb32eb5300831a27d82a071cbc8218994083744
LTM0714 <lim2309043@gmail.com>
2025-11-23 22:07:51 +0900

    ORM(Object Relational Mapping)을 사용해서 데이터베이스 작업2

--- 4-6/domain/question/question_router.py
+++ 4-6/domain/question/question_router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from pydantic import BaseModel # 임시로 추가 삭제할 것
+from pydantic import BaseModel  # 임시로 추가 삭제할 것
 
 from models import Question
 from database import Session as SessionLocal
 from domain.question.question_schema import QuestionSubject
 
 
-router = APIRouter(prefix="/api/question", tags=["question"])
+router = APIRouter(prefix='/api/question')
 
 def get_db():
     db = SessionLocal()     # db 연결
@@ -21,32 +21,31 @@ class QuestionCreate(BaseModel):
     subject: str
     content: str
 
-@router.post("/", response_model=QuestionSubject)
+@router.post('/')
 def create_question(item: QuestionCreate, db: Session = Depends(get_db)):
-    """
+    '''
     단일 Question 레코드 생성
-    body 예시: {"subject": "질문 제목", "content": "질문 내용"}
-    """
+    body 예시: {'subject': '질문 제목', 'content': '질문 내용'}
+    '''
     q = Question(subject=item.subject, content=item.content)
     db.add(q)
     db.commit()
     db.refresh(q)
     return q
 
-@router.get("/")
+@router.get('/')
 def question_list(db: Session = Depends(get_db)):
-    """
+    '''
     Question 테이블 전체 목록 반환
-    """
+    '''
     questions = db.query(Question).all()
     return questions
 
-@router.get("/subject", response_model=list[QuestionSubject])
+
+@router.get('/subject', response_model=list[QuestionSubject])
 def question_list2(db: Session = Depends(get_db)):
     questions = db.query(Question).all()
     return questions
 
 
-# curl -X POST "http://127.0.0.1:8000/api/question/" -H "Content-Type: application/json" -d "{\"subject\":\"첫 질문\",\"content\":\"내용\"}"
-
-
+# curl -X POST 'http://127.0.0.1:8000/api/question/' -H 'Content-Type: application/json' -d "{'subject':'첫 질문','content':'내용'}"
--- 4-6/domain/question/question_schema.py
+++ 4-6/domain/question/question_schema.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-from datetime import datetime
 
 class QuestionSubject(BaseModel):
     # id: int
     subject: str
 
     class Config:
-        orm_mode = True
+        orm_mode = True # ORM 객체를 Pydantic 객체로 변환 가능하게 설정
+
